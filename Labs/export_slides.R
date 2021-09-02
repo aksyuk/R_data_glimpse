2a41d0bf0ee1aeb6a6c4f63280db060a7bfe251c
Svetlana Suyazova (Aksyuk) <s.a.aksuk@gmail.com>
2021-09-02 23:10:09 +0300

    week 01 update
    slides for lab 1 added

--- Labs/export_slides.R
+++ Labs/export_slides.R
@@ -15,5 +15,5 @@ knit(input = './practice_2-1-2021_Getting_Data.Rmd')
 # экспортировать слайды в pdf
 # pagedown::chrome_print("../Slides/slides_practice_01.Rmd", format = 'pdf',
 #                        browser = "C:\\Users\\sa_suyazova\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe")
-pagedown::chrome_print("../Slides/slides_lection_01.Rmd", format = 'pdf',
+pagedown::chrome_print("../Slides/slides_lab_01.Rmd", format = 'pdf',
                        browser = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
